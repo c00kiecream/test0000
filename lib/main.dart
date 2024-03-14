b59fd0b4103388983ce22ddf731da90287d6e281
c00kiecream <stv9450@gmail.com>
2024-03-14 15:29:28 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'App 타이틀',
+//         //App의 전반적인 테마를 설정한다.
+//         theme: ThemeData(
+//           //텍스트의 style을 미리 설정하고 호출해 사용 가능하다.
+//           textTheme: TextTheme(
+//             bodyText1: TextStyle(
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//         ),
+//         home: Scaffold(
+//           appBar: AppBar(
+//             title: Text('AppBar'),
+//             backgroundColor: Colors.blue,
+//           ),
+//           body: Container(
+//             color: Colors.yellow,
+//             child: Center(
+//               child: Text('Body 입니다.'),
+//             ),
+//           ),
+//           // BottomNavigationBar
+//           bottomNavigationBar: BottomNavigationBar(
+//             items: <BottomNavigationBarItem>[
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.home),
+//                 label: 'Home',
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.business),
+//                 label: 'business',
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.school),
+//                 label: 'school',
+//               ),
+//             ],
+//           ),
+//
+//           floatingActionButton: FloatingActionButton(
+//             child: Icon(Icons.access_alarm),
+//             onPressed: () => {
+//               print('hello'),
+//             },
+//           ),
+//         ));
+//   }
+// }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AppBar'),
+          backgroundColor: Colors.blue,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.purple,
+                          child: Text('컨테이너'),
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(20),
+                          padding: EdgeInsets.only(bottom: 20),
+                        ),
+                        Text(
+                          'hello',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.amber),
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.home,
+                      color: Colors.green,
+                      size: 100,
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  'assets/images/cat.jpg',
+                ),
+                Image.network(
+                    'https://gratisography.com/wp-content/uploads/2023/10/gratisography-cool-cat-800x525.jpg'),
+                Image.network(
+                    'https://gratisography.com/wp-content/uploads/2023/05/gratisography-colorful-cat-free-stock-photo-800x525.jpg'),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Input'),
+
+                  //입력폼(text)에 값이 변경될 경우 작동한다.
+                  onChanged: (text) {
+                    print(text);
+                  },
+                  //엔터를 눌렀을 경우 작동한다.
+                  onSubmitted: (text) {
+                    print("enter를 눌렀습니다 입력값: $text");
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
